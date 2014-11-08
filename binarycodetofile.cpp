69414232530825cef73387d83741ccaae4dbaef7
Raideon <luoliangyi@gmail.com>
2014-11-08 18:57:42 +0200

    Update and rename huffmancodetofile.cpp to binarycodetofile.cpp
    Actually, this program can be used to put any binary codes into a file in a compact way. Like we have code length of 998bit than we will get a file of 125bytes. I still don't know how to get rid of the extra 2 bits here, but consider it insignificant when the code is long enough.

--- binarycodetofile.cpp
+++ binarycodetofile.cpp
@@ -2,7 +2,7 @@
 #include <fstream>      
 #include <bitset>              
 
-void huffmancodetobinaryfile(int huffmancodes[], int length){
+void binarycodetobinaryfile(int binarycodes[], int length){
 
 
   const int l = 24; //Unfortunately, we need to hardcode the length (how many bits) of bitstream here. l has to be constant. It also has to be n*8 where 1<=n and n is an integer. For example, if we got huffman codes of 20 bits, here we should put 24. If 999bits then here is 1000.
@@ -10,9 +10,9 @@ void huffmancodetobinaryfile(int huffmancodes[], int length){
 
   std::bitset<l> codesinbits;
 
-  for(int i = 0; i < length; i++){ //read through all integers in huffmancodes array
+  for(int i = 0; i < length; i++){ //read through all integers in binarycodes array
 
-        if( huffmancodes[i]==1 ){ //if position i is 1
+        if( binarycodes[i]==1 ){ //if position i is 1
 
 		codesinbits.set(i); //set the binary bit on position i to be 1
 
@@ -22,7 +22,7 @@ void huffmancodetobinaryfile(int huffmancodes[], int length){
    
     std::cout << "\n";
 
-  std::ofstream output("huffmancode.bin",std::ios::binary); // Actually, what is in this file are huffman codes with some extra zeros. Because the original huffman code migth not be integer times of 8. Hence it is very important that we know the original code length when decoding using this file. Or else, we will decode a few more zeros.
+  std::ofstream output("codesfordecompression.bin",std::ios::binary); // Actually, what is this file are huffman or arithmatic codes with some extra zeros. Because the original length migth not be integer times of 8. Hence it is very important that we know the original code length when decoding using this file. Or else, we will decode a few more zeros.
 
   std::bitset<8> packasbyte; //Or we can say that it will be pack as an unsigned char
   int j = 0;
@@ -49,14 +49,13 @@ void huffmancodetobinaryfile(int huffmancodes[], int length){
 int main ()
 {
 
-  int huffmancodes[] = {0,1,0,0,0,1,1,0,1,1,1,0,1,0,0,1,1}; //just for test. Replace it by real huffman codes for practical use.
+  int huffmancodes[] = {0,1,0,0,0,1,1,0,1,1,1,0,1,0,0,1,1}; //just for test. Replace it by real Huffman codes or arithmatic codes in binary for practical use.
 
   const int length = sizeof(huffmancodes)/sizeof(int);
 
-  std::cout << "Huffman code length: " << length << "\n";
+  std::cout << "Code length: " << length << "bits" << "\n";
 
-  //std::cout << huffmancodes[1];
-  huffmancodetobinaryfile(huffmancodes, length); //This function is what is important. It takes an integer array of huffmancodes as a parameter, and the code length as another parameter. Besides, we have to code the LENGTH OF BITSTREAM SEPAPATELY.
+  binarycodetobinaryfile(huffmancodes, length); //This function is what is important. It takes an integer array of binary as a parameter, and the code length as another parameter. Besides, we have to code the LENGTH OF BITSTREAM SEPAPATELY.
   return 0;
 
 }
