fe32508b615d9e8f16f475bf19abf997d9a646a4
Mikko Rautiainen <m_rautiainen@hotmail.com>
2014-11-08 18:09:59 +0200

    added shortnumber compressor based on smallnumber compressor

---
+++ shortnumber.c
@@ -0,0 +1,87 @@
+/***
+
+Encodes/decodes numbers in text format into 2-byte format. Call "./shortnumber.out c filename" to encode and "./shortnumber.out d filename" to decode.
+
+2-byte format is composed of byte pairs that represent numbers, with two control pairs:
+
+-32768: add 65534 to the next number
+-32767: subtract 65534 from the next number
+
+Any other pair is mapped to a number in the usual way.
+
+***/
+
+#include <stdio.h>
+#include <stdint.h>
+#include <math.h>
+
+void compress(FILE* stream)
+{
+	int number;
+	int16_t tooBig = -32768;
+	int16_t tooSmall = -32767;
+	while (scanf("%d", &number) != EOF)
+	{
+		while (number > 32767)
+		{
+			fwrite(&tooBig, 2, 1, stream);
+			number -= 65534;
+		}
+		while (number < -32766)
+		{
+			fwrite(&tooSmall, 2, 1, stream);
+			number += 65534;
+		}
+		int16_t writeNumber = number;
+		fwrite(&writeNumber, 2, 1, stream);
+	}
+}
+
+void printn(int number)
+{
+	if (number < 0) putchar('-');
+	printf("%d.%02d\n", abs(number/100), abs(number%100));
+}
+
+void decompress(FILE* stream)
+{
+	int number = 0;
+	while (1)
+	{
+		uint16_t a;
+		fread(&a, 2, 1, stream);
+		if (feof(stream))
+		{
+			return;
+		}
+		while ((int16_t)a == -32768)
+		{
+			number += 65534;
+			fread(&a, 2, 1, stream);
+		}
+		while ((int16_t)a == -32767)
+		{
+			number -= 65534;
+			fread(&a, 2, 1, stream);
+		}
+		number += (int16_t)a;
+		printn(number);
+		number = 0;
+	}
+}
+
+int main(int argc, char** argv)
+{
+	if (*argv[1] == 'c')
+	{
+		FILE* fp = fopen(argv[2], "wb");
+		compress(fp);
+		fclose(fp);
+	}
+	else
+	{
+		FILE* fp = fopen(argv[2], "rb");
+		decompress(fp);
+		fclose(fp);
+	}
+}
--- test.sh
+++ test.sh
@@ -1,6 +1,3 @@
-gcc smallnumber.c -o smallnumber.out
-gcc smallnumber_mini.c -o smallnumber_mini.out
-gcc curve.c -o curve.out -lm
-gcc curve_mini.c -o curve_mini.out -lm
-./curve.out c < curve1.dat | ./smallnumber.out c compressed.out
-./smallnumber_mini.out compressed.out | ./curve_mini.out > decompressed.out
+gcc shortnumber.c -o shortnumber.out
+tr -d '.' < curve1.dat | ./shortnumber.out c compressed.out
+./shortnumber.out d compressed.out > decompressed.out
