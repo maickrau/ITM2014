eceb2555df582f2661840fa7d8ae82b3b3c3545f
Mikko Rautiainen <m_rautiainen@hotmail.com>
2014-11-09 17:37:09 +0200

    added missing libraries for compiling at melkki

--- itmProject/compressor/compress.sh
+++ itmProject/compressor/compress.sh
@@ -1,4 +1,4 @@
-gcc compressor/curve.c -o compressor/curve.out
+gcc compressor/curve.c -o compressor/curve.out -lm
 gcc compressor/shortnumber.c -o compressor/shortnumber.out
 tr -d '.' < $1 | ./compressor/curve.out c | ./compressor/shortnumber.out c compressor/temp.temp
 cat compressor/temp.temp
--- itmProject/decompressor/decompress.sh
+++ itmProject/decompressor/decompress.sh
@@ -1,4 +1,4 @@
 gcc decompressor/a.c -o a
-gcc decompressor/b.c -o b
+gcc decompressor/b.c -o b -lm
 gcc decompressor/c.c -o c
 ./c $1 | ./b d | ./a
