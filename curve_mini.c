6e6eb7419af238307fa02ab199c6f34e3c202eb0
Mikko Rautiainen <m_rautiainen@hotmail.com>
2014-11-09 17:03:28 +0200

    added minified versions of shortnumber, curve and addcomma

--- curve_mini.c
+++ curve_mini.c
@@ -1,3 +1,3 @@
 #include <math.h>
 #include <stdio.h>
-double a=3,b=1.57,c=-0.12,d=0.08,e=-0.06,f=0.04;int main(){int g,h;double i=0;while(scanf("%d",&g)!=EOF){i+=1;h=-sin(i/a+b)*(c*(199-i)+d*i)+(e*(199-i)+f*i);h*=h*(h>0?1:-1);g+=h;printf("%d\n",g);}}
+typedef double h;h j(h x,h a,h b,h c,h d,h e,h f,h g){h o=sin(x*a+b)*(c*(2000.0-x)+d*x)/2000.0+(e*(2000.0-x)+f*x)/2000.0;return pow(fabs(o),g)*(o>0?1:-1)*100;}main(){int k,l,i=0;while(scanf("%d",&k)!=EOF){i++;l=j(i,0.0333,1.57,67.13,-63,-49.66,0,1.5)+j(i,0.067,-2.5,350,-300,200,0,1);k=k+l;printf("%d\n",k);}}
--- test.sh
+++ test.sh
@@ -6,6 +6,10 @@ gcc addcomma.c -o addcomma.out
 #or
 tr -d '.' < curve1.dat | ./curve.out c | ./shortnumber.out c compressed.out
 
+gcc curve_mini.c -o curve_mini.out
+gcc addcomma_mini.c -o addcomma_mini.out
+gcc shortnumber_mini.c -o shortnumber_mini.out
+
 #./shortnumber.out d compressed.out | ./addcomma.out > decompressed.out
 #or
-./shortnumber.out d compressed.out | ./curve.out d | ./addcomma.out > decompressed.out
+./shortnumber_mini.out compressed.out | ./curve_mini.out d | ./addcomma_mini.out > decompressed.out
