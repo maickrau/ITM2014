1d8a39346b0eacc4d715910255be1b556b4665d4
Mikko Rautiainen <m_rautiainen@hotmail.com>
2014-11-17 19:43:41 +0200

    fixed scripts

--- itmProject/decompressor/decompress.sh
+++ itmProject/decompressor/decompress.sh
@@ -1,29 +1,24 @@
+#!/bin/bash
 gcc decompressor/numberdiff.c -o numberdiff.out
 g++ decompressor/paleocompresser.cpp -std=c++0x -o paleocompresser.out
-
 a=$(head -c 1 < $1)
-
 dd ibs=1 skip=1 < $1 > fixed.temp
-
 if [ "$a" == "1" ] 
 then
 	./numberdiff.out d fixed.temp > temp.temp
 	cat temp.temp
 fi
-
 if [ "$a" == "2" ] 
 then
 	./paleocompresser.out d fixed.temp temp.temp
 	cat temp.temp
 fi
-
 if [ "$a" == "3" ] 
 then
 	cp fixed.temp stock.c
 	gcc stock.c -o stock.out
 	./stock.out
 fi
-
 if [ "$a" == "4" ] 
 then
 	cat fixed.temp
