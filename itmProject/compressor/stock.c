a4dadf81d3b26c9c008aa094048bce79ec2f07db
Mikko Rautiainen <m_rautiainen@hotmail.com>
2014-11-17 20:28:45 +0200

    minified stock and tar'd the delivery

--- itmProject/compressor/stock.c
+++ itmProject/compressor/stock.c
@@ -1,15 +1,4 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
-
-int main()
-{
-	srand48(713);
-	uint16_t num = lrand48();
-	int i = 0;
-	for(;i<100;i++)
-	{
-		num += lrand48()%400+lrand48()%400-395;
-		fwrite(&num, 2, 1, stdout);
-	}
-}
+int main(){srand48(713);uint16_t n=lrand48();int i=0;for(;i<100;i++){n+=lrand48()%400+lrand48()%400-395;fwrite(&n,2,1,stdout);}}
