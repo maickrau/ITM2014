30fa63aaba4ea59735a75090ea212cb417504cad
Mikko Rautiainen <m_rautiainen@hotmail.com>
2014-11-25 20:03:49 +0200

    made tar for week 3

--- itmProject/decompressor/numberdiff.c
+++ itmProject/decompressor/numberdiff.c
@@ -1,4 +1,4 @@
 
 #include <stdio.h>
 #include <stdint.h>
-main(int h,char**i){FILE*fp=fopen(i[2],"rb");f=*i[3]=='y';int16_t c;int32_t b=0,d=0;fread(&c,2,1,e);while(!feof(e)){if(c==-32767){d=0;fread(&d,4,1,e);b+=d;}else{b+=c;}if(f){if(b<0)putchar('-');printf("%d.%02d\n",abs(b/100),abs(b%100));}else{double g=b/100.0;printf("%g\n",g);}fread(&c,2,1,e);}}
+main(int h,char**i){FILE*e=fopen(i[2],"rb");int f=*i[3]=='y';int16_t c;int32_t b=0,d=0;fread(&c,2,1,e);while(!feof(e)){if(c==-32767){d=0;fread(&d,4,1,e);b+=d;}else{b+=c;}if(f){if(b<0)putchar('-');printf("%d.%02d\n",abs(b/100),abs(b%100));}else{double g=b/100.0;printf("%g\n",g);}fread(&c,2,1,e);}}
