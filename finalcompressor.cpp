fabd9ad6950d525ae4f358f506711850f9fb2ec3
Mikko Rautiainen <m_rautiainen@hotmail.com>
2014-12-09 18:47:56 +0200

    compressor for final data

--- finalcompressor.cpp
+++ finalcompressor.cpp
@@ -3,6 +3,100 @@
 #include <cstring>
 #include <iostream>
 #include <vector>
+#include <cassert>
+
+class BitWriter
+{
+public:
+	BitWriter(FILE* s) : totalWritten(0), stream(s), bitLoc(0), nextBits(0) {};
+	~BitWriter()
+	{
+		flushWrite();
+	};
+	void writeBits(char* target, int bits);
+	size_t written()
+	{
+		return totalWritten;
+	};
+private:
+	size_t totalWritten;
+	void flushWrite();
+	FILE* stream;
+	int bitLoc;
+	char nextBits;
+};
+
+class BitReader
+{
+public:
+	BitReader(FILE* s) : stream(s), bitLoc(0), nextBits(0) {};
+	void readBits(char* target, int bits);
+	bool eof()
+	{
+		return feof(stream);
+	};
+private:
+	FILE* stream;
+	int bitLoc;
+	int nextBits;
+};
+
+void BitReader::readBits(char* target, int bits)
+{
+	int written = 0;
+	while (written < bits)
+	{
+		if (bitLoc == 0)
+		{
+			nextBits = fgetc(stream);
+		}
+		if (written % 8 == 0)
+		{
+			*target = 0;
+		}
+		*target |= (nextBits&(1<<bitLoc) ? 1 : 0) << written%8;
+		bitLoc++;
+		bitLoc %= 8;
+		written++;
+		if (written % 8 == 0)
+		{
+			target++;
+		}
+	}
+}
+
+void BitWriter::writeBits(char* target, int bits)
+{
+	totalWritten += bits;
+	int read = 0;
+	while (read < bits)
+	{
+		if (bitLoc == 0)
+		{
+			nextBits = 0;
+		}
+		nextBits |= ((*target)&(1<<(read%8)) ? 1 : 0) << bitLoc;
+		bitLoc++;
+		bitLoc %= 8;
+		if (bitLoc == 0)
+		{
+			fputc(nextBits, stream);
+		}
+		read++;
+		if (read % 8 == 0)
+		{
+			target++;
+		}
+	}
+}
+
+void BitWriter::flushWrite()
+{
+	if (bitLoc != 0)
+	{
+		fputc(nextBits, stream);
+	}
+}
 
 class BezierSurface
 {
@@ -30,6 +124,10 @@ public:
 		ret += (x)*(x)*(y)*(y)*height[8];
 		return ret;
 	};
+	int getValueInt(double xAbsolute, double yAbsolute)
+	{
+		return getValue(xAbsolute, yAbsolute)*1000;
+	}
 	void fit(const std::vector<std::vector<double>>& values, int iterations, double wildness, double decay)
 	{
 		for (int iter = 0; iter < iterations; iter++)
@@ -72,6 +170,17 @@ public:
 		return ret;
 	}
 
+	std::vector<double> differences(const std::vector<std::vector<double>>& values)
+	{
+		std::vector<double> ret;
+		for (int i = 0; i < values.size(); i++)
+		{
+			double diff = getValue(values[i][0], values[i][1])-values[i][2];
+			ret.push_back(diff);
+		}
+		return ret;
+	}
+
 	double height[9];
 };
 
@@ -86,13 +195,9 @@ int classify(double x, double y, double z)
 {
 	if (divide(x, y, z))
 	{
-		if (y > 0.45)
-		{
-			return 1;
-		}
 		return 0;
 	}
-	return 2;
+	return 1;
 }
 
 std::vector<std::vector<double>> loadData(std::string dataFile, std::string sideFile)
@@ -117,15 +222,76 @@ std::vector<std::vector<double>> loadData(std::string dataFile, std::string side
 	return ret;
 }
 
+std::vector<int> loadDataExact(std::string dataFile)
+{
+	std::ifstream data {dataFile};
+	std::vector<int> ret;
+	for (int i = 0; i < 20000; i++)
+	{
+		int sign = 1;
+		while (data.peek() != '-' && (data.peek() < '0' || data.peek() > '9'))
+		{
+			data.get();
+		}
+		if (data.peek() == '-')
+		{
+			sign = -1;
+			data.get();
+		}
+		int left = 0, right = 0;
+		data >> left;
+		if (data.peek() == '.')
+		{
+			char a = data.get();
+			a = data.get();
+			right = 100*(a-'0');
+			if (data.peek() != '\n')
+			{
+				a = data.get();
+				right += 10*(a-'0');
+				if (data.peek() != '\n')
+				{
+					a = data.get();
+					right += (a-'0');
+				}
+			}
+		}
+		if (sign < 0)
+		{
+			left *= -1;
+			right *= -1;
+		}
+		ret.push_back(left*1000+right);
+	}
+	return ret;
+}
+
+std::vector<std::vector<double>> loadSideData(std::string sideFile)
+{
+	std::ifstream side {sideFile};
+
+	std::vector<std::vector<double>> ret;
+
+	for (int i = 0; i < 20000; i++)
+	{
+		double x, y;
+		side >> x;
+		side >> y;
+		ret.emplace_back();
+		ret.back().push_back(x);
+		ret.back().push_back(y);
+	}
+
+	return ret;
+}
+
 void findOptimalMode(char** argv)
 {
 	std::cout << "load data\n";
 	auto data = loadData(argv[1], argv[2]);
 	std::vector<std::vector<double>> topData;
-	std::vector<std::vector<double>> middleData;
 	std::vector<std::vector<double>> bottomData;
 	BezierSurface topSurface;
-	BezierSurface middleSurface;
 	BezierSurface bottomSurface;
 	std::cout << "split\n";
 	for (int i = 0; i < data.size(); i++)
@@ -135,10 +301,6 @@ void findOptimalMode(char** argv)
 		{
 			topData.push_back(data[i]);
 		}
-		else if (type == 1)
-		{
-			middleData.push_back(data[i]);
-		}
 		else
 		{
 			bottomData.push_back(data[i]);
@@ -147,14 +309,10 @@ void findOptimalMode(char** argv)
 	std::cout << "cull\n";
 	auto topDataPart = topData;
 	topDataPart.erase(topDataPart.begin()+500, topDataPart.end());
-	auto middleDataPart = middleData;
-	middleDataPart.erase(middleDataPart.begin()+500, middleDataPart.end());
 	auto bottomDataPart = bottomData;
 	bottomDataPart.erase(bottomDataPart.begin()+500, bottomDataPart.end());
 	std::cout << "fit top\n";
 	topSurface.fit(topDataPart, 1000, 1, 0.99);
-	std::cout << "fit middle\n";
-	middleSurface.fit(middleDataPart, 1000, 1, 0.99);
 	std::cout << "fit bottom\n";
 	bottomSurface.fit(bottomDataPart, 1000, 1, 0.99);
 	std::cout << "scores\n";
@@ -165,13 +323,6 @@ void findOptimalMode(char** argv)
 		std::cout << topSurface.height[i] << "\n";
 	}
 	std::cout << "\n";
-	std::cout << middleSurface.getScore(middleData) << "\n";
-	std::cout << middleSurface.maxOffset(middleData) << "\n";
-	for (int i = 0; i < 9; i++)
-	{
-		std::cout << middleSurface.height[i] << "\n";
-	}
-	std::cout << "\n";
 	std::cout << bottomSurface.getScore(bottomData) << "\n";
 	std::cout << bottomSurface.maxOffset(bottomData) << "\n";
 	for (int i = 0; i < 9; i++)
@@ -179,9 +330,138 @@ void findOptimalMode(char** argv)
 		std::cout << bottomSurface.height[i] << "\n";
 	}
 	std::cout << "\n";
+
+	std::ofstream out{argv[3]};
+	auto diffs = topSurface.differences(topData);
+	for (int i = 0; i < diffs.size(); i++)
+	{
+		out << diffs[i] << "\n";
+	}
+	diffs = bottomSurface.differences(bottomData);
+	for (int i = 0; i < diffs.size(); i++)
+	{
+		out << diffs[i] << "\n";
+	}
+}
+
+BezierSurface fit(std::vector<std::vector<double>> data, int points)
+{
+	data.erase(data.begin()+points, data.end());
+	BezierSurface surface;
+	surface.fit(data, 1000, 1, 0.99);
+	return surface;
+}
+
+void encodeDifferencesMode(char** argv)
+{
+	std::vector<std::vector<double>> topData;
+	std::vector<std::vector<double>> bottomData;
+	std::vector<int> dataExact = loadDataExact(argv[1]);
+	auto data = loadData(argv[1], argv[2]);
+	for (int i = 0; i < data.size(); i++)
+	{
+		int type = classify(data[i][0], data[i][1], data[i][2]);
+		if (type == 0)
+		{
+			topData.push_back(data[i]);
+		}
+		else
+		{
+			bottomData.push_back(data[i]);
+		}
+	}
+	BezierSurface top = fit(topData, 1000);
+	BezierSurface bottom = fit(bottomData, 1000);
+
+	FILE* writeFile = fopen(argv[3], "wb");
+	BitWriter writer {writeFile};
+
+	for (int i = 0; i < 9; i++)
+	{
+		writer.writeBits((char*)&top.height[i], sizeof(double)*8);
+		writer.writeBits((char*)&bottom.height[i], sizeof(double)*8);
+		std::cerr << bottom.height[i] << "\n";
+	}
+
+	for (int i = 0; i < data.size(); i++)
+	{
+		int classification = classify(data[i][0], data[i][1], data[i][2]);
+		int measure;
+		if (classification == 0)
+		{
+			measure = top.getValueInt(data[i][0], data[i][1]);
+		}
+		else
+		{
+			measure = bottom.getValueInt(data[i][0], data[i][1]);
+		}
+		int dataHeight = dataExact[i];
+		int diff = dataHeight-measure;
+		writer.writeBits((char*)&classification, 1);
+		int sign = diff < 0;
+		writer.writeBits((char*)&sign, 1);
+		if (diff < 0)
+		{
+			diff *= -1;
+		}
+		assert(diff < 4096);
+		writer.writeBits((char*)&diff, 12);
+	}
+	fclose(writeFile);
+}
+
+void decodeDifferencesMode(char** argv)
+{
+	BezierSurface top;
+	BezierSurface bottom;
+	FILE* readFile = fopen(argv[3], "rb");
+	BitReader reader {readFile};
+
+	auto sideData = loadSideData(argv[2]);
+
+	for (int i = 0; i < 9; i++)
+	{
+		reader.readBits((char*)&top.height[i], sizeof(double)*8);
+		reader.readBits((char*)&bottom.height[i], sizeof(double)*8);
+		std::cerr << bottom.height[i] << "\n";
+	}
+
+	for (int i = 0; i < 20000; i++)
+	{
+		int classification = 0;
+		int sign = 0;
+		int n = 0;
+		reader.readBits((char*)&classification, 1);
+		reader.readBits((char*)&sign, 1);
+		reader.readBits((char*)&n, 12);
+		if (sign)
+		{
+			n *= -1;
+		}
+		int measure;
+		if (classification == 0)
+		{
+			measure = top.getValueInt(sideData[i][0], sideData[i][1]);
+		}
+		else
+		{
+			measure = bottom.getValueInt(sideData[i][0], sideData[i][1]);
+		}
+		int height = measure+n;
+		double heightD = height/1000.0;
+		printf("%1.3f\n", heightD);
+	}
+	fclose(readFile);
 }
 
 int main(int argc, char** argv)
 {
-	findOptimalMode(argv);
+	if (*argv[4] == 'c')
+	{
+		encodeDifferencesMode(argv);
+	}
+	else
+	{
+		decodeDifferencesMode(argv);
+	}
 }
