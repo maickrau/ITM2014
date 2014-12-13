0e46fd780ea2fa614b26c34893e6c61e8104543a
Raideon <luoliangyi@gmail.com>
2014-12-13 18:34:09 +0200

    Update tydc.m

--- tydc.m
+++ tydc.m
@@ -1,3 +1,5 @@
+%Needs matlab/octave function typeA1f.m and basef.m, where contain the models
+
 tyin=load('ty.out');
 
 result=0;
@@ -6,20 +8,19 @@ f = fopen('tyftest.txt', 'w+');
 
 for i = 1:10000;
 
-	if (201<=i && i<=749)||(1151<=i && i<=1699) ||(2101<=i && i<=2649) ||(3051<=i && i<=3549) ||(3951<=i && i<=4499) ||(4901<=i && i<=5399); %typeA1
-		result=tyin(i)+(basef(i)+typeA1f(i-200)+50);
+	if (201<=i && i<=749)||(1151<=i && i<=1699) ||(2101<=i && i<=2649) ||(3051<=i && i<=3549) ||(3951<=i && i<=4499); %typeA1
+		result=tyin(i)+(7*basef(i)+typeA1f(i-200)+30);
 
-	elseif (7301<=i && i<=7499)||(7601<=i && i<=7799) ||(8221<=i && i<=8419)||(8541<=i && i<=8779) ||(9151<=i && i<=9359) ||(9481<=i && i<=9719); %typeA2
-		result=tyin(i)+(basef(i)+typeA2f(i)+50);
+	elseif (1<=i && i<=200)||(750<=i && i<=1150)||(1700<=i && i<=2100)||(2650<=i && i<=3050)||(3550<=i && i<=3950)||(4500<=i && i<=4900)||(5400<=i && i<=5750)||(6500<=i && i<=6650)||(7450<=i && i<=7600)||(8420<=i && i<=8540)||(9360<=i && i<=9480);
+		result=tyin(i)+(7*basef(i)+30);
 
-        elseif (5751<=i && i<=6499)||(6651<=i && i<=7300) ||(7800<=i && i<=8220) ||(8780<=i && i<=9150) ||(9720<=i && i<=10000);  %typeC
-		result=tyin(i)+(basef(i)+typeCf(i)+50);
+	else; result=tyin(i);
 
-	else;
-		result=tyin(i)+(basef(i)+50);
 	endif;
 
-	fprintf(f, '%#.2f', result);
+
+
+	fprintf(f, '%#.2f', result); %Needs last zeros removal
 	fprintf(f, '\n');
 
 endfor;
